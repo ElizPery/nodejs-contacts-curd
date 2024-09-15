505a659fe8ba516793c882d0c403e2112e00a37a
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-09-15 18:30:32 +0200

    add data validation of post request

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import * as contactsControllers from '../controllers/contacts.js';
 import ctrlWrapper from "../utils/ctrlWrapper.js";
+import validateBody from "../utils/validateBody.js";
+import { createContactsSchema } from "../validation/contacts.js";
 
 const contactsRouter = Router();
 
@@ -9,7 +11,7 @@ contactsRouter.get('/', ctrlWrapper(contactsControllers.getAllContactsController
 
 contactsRouter.get('/:contactId', ctrlWrapper(contactsControllers.getContactByIdController));
 
-contactsRouter.post('/', ctrlWrapper(contactsControllers.addContactController));
+contactsRouter.post('/', validateBody(createContactsSchema), ctrlWrapper(contactsControllers.addContactController));
     
 contactsRouter.patch('/:contactId', ctrlWrapper(contactsControllers.patchContactController));
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,9 +1,10 @@
 import Joi from 'joi';
+import { contactTypeList } from '../constants/contacts.js';
 
 export const createContactsSchema = Joi.object({
-    name: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
-    email: Joi.string().email(),
+    name: Joi.string().min(3).max(20).required(),
+    phoneNumber: Joi.string().min(3).max(20).required(),
+    email: Joi.string().min(3).max(20).email(),
     isFavourite: Joi.boolean(),
-    contactType: Joi.string().valid('work', 'home', 'personal').required()
+    contactType: Joi.string().valid(...contactTypeList).required()
 });
